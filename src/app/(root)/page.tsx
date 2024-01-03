9ea401864694b446e2d8bf90e7294aaf57811e6c
unknown <griel034@gmail.com>
2024-01-03 15:52:23 +0800

    Add Change in dashboard

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
 
+import Link from 'next/link';
+import { BiLogoMicrosoft } from "react-icons/bi";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-<h1>HOME</h1>
+<Link href="#" className="flex items-center space-x-2 rtl:space-x-reverse">
+      <span className='text-4xl text-white rotate-45 bg-violet-600 p-2 rounded-full'><BiLogoMicrosoft /></span>
+      <span className="self-center text-violet-600 text-6xl font-bold whitespace-nowrap dark:text-white">OneLot</span>
+  </Link>
     </div>
   )
 }
